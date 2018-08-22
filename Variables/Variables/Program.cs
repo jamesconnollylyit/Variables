52f97988a186f209b23770ef5f8fc931ca0ee890
James Connolly <james.connolly@lyit.ie>
2018-08-22 15:34:01 +0100

    Changes made before enum variable type

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float pi = 3.1415926f;
+
+            double anotherPi = 3.14159265358979323846;
+
+            decimal number = 1.495m;
+
+            number = 14.4m;
+
+            char myInitial = 'J';
+
+            bool itWorked = true;
+
+            itWorked = false;
+
+            string message = "Hello World";
+
+            message = "Turkey and ham";
+
+
+            Console.WriteLine($"Content of myInitial is {myInitial}");
+            
+            Console.ReadKey();
+
+        }
+
+       
+
+    }
+}

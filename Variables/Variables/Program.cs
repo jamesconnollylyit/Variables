416cfe57ef78b5a228a92d5357aa28bae09edf88
James Connolly <james.connolly@lyit.ie>
2018-08-24 10:59:14 +0100

    Added List code

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,33 +9,110 @@ namespace Variables
 {
     class Program
     {
-        static void Main(string[] args)
+        enum Orientation : int
         {
-            float pi = 3.1415926f;
-
-            double anotherPi = 3.14159265358979323846;
-
-            decimal number = 1.495m;
-
-            number = 14.4m;
-
-            char myInitial = 'J';
-
-            bool itWorked = true;
-
-            itWorked = false;
-
-            string message = "Hello World";
-
-            message = "Turkey and ham";
-
-
-            Console.WriteLine($"Content of myInitial is {myInitial}");
-            
-            Console.ReadKey();
-
+            north = 1,
+            south = 2,
+            east = 3,
+            west = 4
         }
 
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            people.Add(new Person
+            {
+                FirstName = "James",
+                SurName = "Connolly",
+                HouseNo = 4,
+                Street = "Main Street",
+                Town = "Letterkenny",
+                County = "Donegal"
+            });
+
+            people.Add(new Person
+            {
+                FirstName = "Joe",
+                SurName = "Soap",
+                HouseNo = 56,
+                Street = "High Street",
+                Town = "Letterkenny",
+                County = "Donegal"
+            });
+
+           
+            List<Person> secondList = new List<Person>();
+            secondList.Add(new Person
+            {
+                FirstName = "John",
+                SurName = "Doe",
+                HouseNo = 56,
+                Street = "Main Street",
+                Town = "Letterkenny",
+                County = "Donegal"
+            });
+
+            people.AddRange(secondList);
+
+            //people.Remove(new Person
+            //{
+            //    FirstName = "John",
+            //    SurName = "Doe",
+            //    HouseNo = 56,
+            //    Street = "Main Street",
+            //    Town = "Letterkenny",
+            //    County = "Donegal"
+            //});
+
+            Console.WriteLine(people.Contains(new Person
+            {
+                FirstName = "John",
+                SurName = "Doe",
+                HouseNo = 56,
+                Street = "Main Street",
+                Town = "Letterkenny",
+                County = "Donegal"
+            }));
+
+
+            //people.Remove(new Person
+            //{
+            //    FirstName = "John",
+            //    SurName = "Doe",
+            //    HouseNo = 56,
+            //    Street = "Main Street",
+            //    Town = "Letterkenny",
+            //    County = "Donegal"
+            //});
+
+            people.RemoveAll(t => t.FirstName == "John");
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"Person firstname is : {person.FirstName}");
+                Console.WriteLine($"Person surname is : {person.SurName}");
+                Console.WriteLine($"Person HouseNo is : {person.HouseNo}");
+                Console.WriteLine($"Person Street is : {person.Street}");
+                Console.WriteLine($"Person Town is : {person.Town}");
+                Console.WriteLine($"Person County is : {person.County}");
+            }
+
+            Console.ReadKey();
+                      
+           
+        }
+
+      
+
+        struct Person
+        {
+            public string FirstName;
+            public string SurName;
+            public string Street;
+            public string Town;
+            public string County;
+            public int HouseNo;
+        }
        
 
     }
